fde0887f24716416af738f485466d08584113432
ruslana-ua <62610250+ruslana-ua@users.noreply.github.com>
2020-03-24 18:19:58 +0200

    Giveaway

---
+++ js/main.js
@@ -0,0 +1,58 @@
+// alert ('Привет, мир!');
+
+
+// $(function () {
+	$(window).scroll(function() {
+	    $('.futures').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+
+$(window).scroll(function() {
+	    $('.section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("swing");
+	        }
+	    });
+	});
+
+$(window).scroll(function() {
+	    $('.title2').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("swing");
+	        }
+	    });
+	});
+
+$(window).scroll(function() {
+	    $('form').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+
+$(window).scroll(function() {
+	    $('.footer').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
